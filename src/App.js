f44c491609e6f1be3b49e14deb0caf929916a937
lukenykitongo <lukenypinto2021@gmail.com>
2022-05-25 12:46:39 +0100

    redenrizando listas flhei tnha bag

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  
+
  function App() {
-
-  return (  
-    <div>
-      
-    </div>
-
-
+  return ( 
+    <ul>{listItems}
+      <ShoppingList/>
+      </ul>
   );
 }
 
