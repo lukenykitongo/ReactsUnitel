68f1da933cbd691e046b6fbec3ab5ae9f8b48b76
lukenykitongo <lukenypinto2021@gmail.com>
2022-05-25 13:08:44 +0100

    click two

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import './App.css';
 
 import { useState } from 'react';
 
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
+function MyButton({ count, onClick }) {
   return (
-    <button onClick={handleClick}>
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
 }
 
  function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return ( 
     <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
+    <h1>Counters that update together</h1>
+    <MyButton count={count} onClick={handleClick} />
+    <MyButton count={count} onClick={handleClick} />
+  </div>
+);
 }
+
 
 export default App;
