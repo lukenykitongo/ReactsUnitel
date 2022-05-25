59cb6495fd312f727172bfb24a8a5c470581600b
lukenykitongo <lukenypinto2021@gmail.com>
2022-05-25 12:58:35 +0100

    clicks one

--- src/App.js
+++ src/App.js
@@ -3,33 +3,30 @@ import './App.css';
 
 
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
+import { useState } from 'react';
 
-function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
+function MyButton() {
+  const [count, setCount] = useState(0);
 
-  
+  function handleClick() {
+    setCount(count + 1);
+  }
 
- function App() {
-  return ( 
-    <ul>{listItems}
-      <ShoppingList/>
-      </ul>
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
 }
 
+ function App() {
+  return ( 
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
 
 export default App;
